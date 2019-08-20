46d5b51a039e873470f8e45c8f81c70bcf4a89fc
Christian Maniewski <chmanie@fastmail.com>
2019-08-20 15:48:41 +0200

    Add exceptions for long import/export statements

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ module.exports = {
   plugins: ['flowtype', 'prettier'],
   rules: {
     'eqeqeq': ['error', 'smart'],
-    'max-len': ['error', { code: 80, ignoreComments: true }],
+    'max-len': ['error', {
+      code: 80,
+      // Allow import, export and implements statements. Also long backticks
+      ignorePattern: '^import [^,]+ from |^export | implements | `.{30,}`',
+      ignoreComments: true,
+    }],
     'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
     'no-use-before-define': ['error', { functions: false, 'classes': true }],
